f972dfa04d27eda0972efcdfa6ce7d063f132b33
Ravi Teja Tatipamula <teja2021@gmail.com>
2017-10-11 23:19:52 -0700

    Create virtual_inheritence.cpp

---
+++ virtual_inheritence.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+using namespace std;
+
+ class Marks{
+ int intmark;
+ int extmark;
+
+ };
+
+
+int main(){
+//    Person anil;
+//    Person::Address ad;
+//    anil.name ="anil";
+//    anil.addr.country="US";
+//    anil.addr.stname = "South";
+//    anil.addr.hno = 868;
+//
+//    anil.adresspls();
+
+
+
+return 0;
+}
+
+
+
+//class Person{
+// public:
+//     string name;
+//
+//    class Address{
+//    public:
+//        string country;
+//        string stname;
+//        int hno;
+//     };
+//
+//     Address addr;
+//
+//     void adresspls(){
+//     cout<<name<<endl<<addr.country<<endl<<addr.stname<<endl<<addr.hno<<endl;
+//     }
+// };
+
+//class Animal{
+//public:
+//    int age;
+//    void walk(){
+//    cout<<"Animal Walks"<<endl;
+//    }
+//
+//    Animal(){
+//    cout<<"Animal constructor"<<endl;
+//    }
+//};
+//
+//class Tiger :virtual public Animal{
+//public:
+//
+//    Tiger(){
+//    cout<<"Tiger Constructor"<<endl;
+//    }
+//};
+//
+//class Lion :virtual public Animal{
+//public:
+//    Lion(){
+//    cout<<"Lion Constructor"<<endl;
+//    }
+//};
+//
+//class Liger : public Lion, public Tiger{
+//public:
+//    Liger(){
+//    cout<<"Liger Constructor"<<endl;
+//    }
+//};
+////class Student : public Person{
+////public:
+////    void introduce(){
+////    cout<<"Hi from Student"<<endl;
+////    }
+////};
+//////
+//////class GStudent: public Student{
+//////
+//////};
+////
+////void whoIsthis(Person &p){
+////p.introduce();
+////}
+//
+//int main()
+//{
+//    Liger anil;
+//    anil.walk();
+//
+//    return 0;
+//}
